c89a5ee7bf0086373a9ac71ef73549f4f3c5e1a9
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-05-15 01:28:09 +0100

    Add new friend

--- friends.rb
+++ friends.rb
@@ -6,6 +6,10 @@ def get_favourite_tv_show(person)
   return person[:favourites][:tv_show]
 end
 
-def  likes_to_eat(person, food)
+def likes_to_eat(person, food)
   return person[:favourites][:snacks].include?(food)
+end
+
+def add_new_friend(person, friend)
+  person[:friends].push(friend)
 end
--- specs/friends_spec.rb
+++ specs/friends_spec.rb
@@ -96,6 +96,10 @@ class TestFriends < MiniTest::Test
   # 4. For a given person, add a new name to their list of friends
   # (e.g. the function add_friend(@person2, "Scrappy-Doo") should add Scrappy-Doo to the friends.)
   # (hint: This function should not return anything. After the function call, check for the length of the friends array to test it!)
+  def test_add_new_friend
+    array_length = add_new_friend(@person1, "keara").length
+    assert_equal(5, array_length)
+  end
 
 
   # 5. For a given person, remove a specific name from their list of friends
